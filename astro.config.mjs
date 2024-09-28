29afd7d553d8cb1a35e6b6583e504acce584ee05
Diegocaceres97 <diegocaceres1997garcia@gmail.com>
2024-09-27 21:41:11 -0500

    chore: env and email send added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  experimental: {
+    env: {
+      schema: {
+        FORM_ID: envField.string({
+          context: 'client',
+          access: 'public',
+          optional: false,
+        }),
+      }
+    }
+  }
 });
