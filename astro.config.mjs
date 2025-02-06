2c1138f40fbf0a3da48d089a1712c40993bd946a
Diegocaceres97 <diegocaceres1997garcia@gmail.com>
2025-02-05 22:33:23 -0500

    chore: dark toggle

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,12 @@ import tailwind from '@astrojs/tailwind';
 
 import vercel from '@astrojs/vercel/serverless';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
 
   experimental: {
   },
